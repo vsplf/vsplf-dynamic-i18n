34eb095b4ca323c55d9cd8fcf895de0d3b520037
Hoat Le <hoatlevan@gmail.com>
2012-02-22 01:21:58 +0700

    Add vsplf-dynamic-i18n-api module

---
+++ vsplf-dynamic-i18n-api/src/main/java/org/vsplf/i18n/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * I18N framework for VSPLF Foundation Projects. This can be used for any I18n Apps.
+ * <ul>
+ *   <li>Leverage Java Translator</li>
+ *   <li>https://wiki.jenkins-ci.org/display/JENKINS/Internationalization</li>
+ *   <li>Similar to http://www.slideshare.net/drupalcon/localize-drupal-org</li>
+ *   <li>http://www.lingoport.com/software-internationalization-products/globalyzer-3/</li>
+ *   <li>http://code.google.com/p/gettext-commons/</li>
+ *   <li>http://www.gnu.org/savannah-checkouts/gnu/gettext/manual/html_node/Java.html</li>
+ *   <li>http://drupal.org/project/i18n</li>
+ *   <li>http://evolvingweb.ca/story/drupal-7-multilingual-whats-new-i18n</li>
+ *   <li>http://drupal.org/documentation/modules/locale</li>
+ *   <li>http://groups.drupal.org/i18n</li>
+ *   <li>http://commons.forgerock.org/i18n-framework/</li>
+ *   <li>https://github.com/zanata/zanata</li>
+ *   <li>http://code.google.com/p/gettext-js/</li>
+ *   <li>http://drupal.org/node/322729</li>
+ * </ul>
+ */
+package org.vsplf.i18n;
